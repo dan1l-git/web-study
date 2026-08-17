93095a5d9b999c6394f2e3b246c1d59489f302bc
dan1l-git <danilgavriluk756@gmail.com>
2026-08-17 13:48:11 +0300

    feat: add bcrypt password hashing utilities

---
+++ 03_jwt/api/utils/hash.py
@@ -0,0 +1,7 @@
+import bcrypt
+
+def get_password_hash(password: str):
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+def verify_password(plain_password: str, hashed_password: str):
+    return bcrypt.checkpw(plain_password.encode(), hashed_password.encode())
